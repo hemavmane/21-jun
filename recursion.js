3707fb45d7da994222e42301d93e42691aefd24a
hemamane <hemamane5376@gmail.com>
2022-06-22 22:10:28 +0530

    recursion

---
+++ recursion.js
@@ -0,0 +1,31 @@
+// function y(num){
+//       console.log(num)
+//       // if(num>0){
+//         return y(num)
+//       }
+     
+//     y(6)
+
+
+    function y(num){
+        //   console.log(num)
+          if(num>0){
+            return y(num-1)
+          }
+         
+          
+          }
+        console.log(6)
+
+
+        
+// function y(num, fact){
+
+//   if(num>0){
+//     fact=fact*num;
+//     return y(num-1,fact)
+    
+//   }return fact;
+// }
+// console.log(y(6,1));
+
--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<question3>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-let amPm = (hr, min) => {
-  if (hr == 0) {
-    min = min < 10 ? "0" + min : min;
-    hr = hr < 10 ? "0" + hr : hr;
-    return "12:" + min + " AM";
-  } else if (hr >= 12) {
-    hr = hr - 12;
-    min = min < 10 ? "0" + min : min;
-    hr = hr < 10 ? "0" + hr : hr;
-    if (hr == 0) {
-      return "12:" + min + " PM";
-    } else {
-      return hr + ":" + min + " PM";
-    }
-  } else {
-    min = min < 10 ? "0" + min : min;
-    hr = hr < 10 ? "0" + hr : hr;
-    return hr + ":" + min + " AM";
-  }
-};
-console.log(amPm(17, 6));
+// // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<question3>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// let amPm = (hr, min) => {
+//   if (hr == 0) {
+//     min = min < 10 ? "0" + min : min;
+//     hr = hr < 10 ? "0" + hr : hr;
+//     return "12:" + min + " AM";
+//   } else if (hr >= 12) {
+//     hr = hr - 12;
+//     min = min < 10 ? "0" + min : min;
+//     hr = hr < 10 ? "0" + hr : hr;
+//     if (hr == 0) {
+//       return "12:" + min + " PM";
+//     } else {
+//       return hr + ":" + min + " PM";
+//     }
+//   } else {
+//     min = min < 10 ? "0" + min : min;
+//     hr = hr < 10 ? "0" + hr : hr;
+//     return hr + ":" + min + " AM";
+//   }
+// };
+// console.log(amPm(17, 6));
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<timely>>>>>>>>>>>>>>>>>>>>>>>>>>>>>...
+// function time(seconds){
+//   let hours = Math.floor(seconds/3600);
+//     let min = Math.floor(seconds/60)-hours*60;
+//     let second = seconds%60;
+//     return (hours+":"+min+":"+second)
+    
+//   }
+//   console.log(time(5000))
+//   exports.time = time
+
+
+// f
